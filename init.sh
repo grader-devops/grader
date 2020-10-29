3da9188d1c27c83cbb507be986c69de45a0e1c6b
Владислав Яблоков <grader@MacBook-Pro-Vladislav.local>
2020-10-29 21:41:05 +0300

    add new docs and text

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo hello world!!!
-git status
-git log 
-git show
-git commit -m 
-git diff 
+git status #посмотреть состояние 
+git log #показать историю коммитов 
+git show #показать последний коммит
+git add . #добавить файлы в изменения 
+git commit -m # добавить коммит 
+git diff <commit1> <commit2> #сравнить изменения в коммитах
+git diff <file> HEAD~1... #покать изменения в этом коммите
+git diff --name-only HEAD~1 <commit1> #показать файл в котором были изменения
+
+git commit --amend #изменить последний коммит
